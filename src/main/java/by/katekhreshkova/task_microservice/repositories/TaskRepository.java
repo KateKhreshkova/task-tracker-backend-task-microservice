80109f3b2d417c8fd3621cd9f90050b99d34ba1f
Kate Khreshkova <katahrnskova@gmail.com>
2026-01-15 18:27:18 +1300

    fix(task): added repository annotation for bean injection

--- src/main/java/by/katekhreshkova/task_microservice/repositories/TaskRepository.java
+++ src/main/java/by/katekhreshkova/task_microservice/repositories/TaskRepository.java
@@ -2,8 +2,10 @@ package by.katekhreshkova.task_microservice.repositories;
 
 import by.katekhreshkova.task_microservice.models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
 }
