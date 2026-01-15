54f895ef7a8c6b6d934422439d9308a00c6c6f80
Kate Khreshkova <katahrnskova@gmail.com>
2026-01-15 19:39:25 +1300

    feat(tasks): add task controller endpoints without implementation

--- src/main/java/by/katekhreshkova/task_microservice/controllers/TaskController.java
+++ src/main/java/by/katekhreshkova/task_microservice/controllers/TaskController.java
@@ -1,9 +1,14 @@
 package by.katekhreshkova.task_microservice.controllers;
 
+import by.katekhreshkova.task_microservice.contracts.req.TaskRequest;
+import by.katekhreshkova.task_microservice.contracts.resp.TaskResponse;
 import by.katekhreshkova.task_microservice.services.TaskService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
     private final TaskService taskService;
 
+    @GetMapping
+    public List<TaskResponse> list(@RequestHeader("X-User-Id") @NotNull final UUID userId){
+        return null;
+    }
+
+    @PostMapping
+    public TaskResponse create(@RequestHeader("X-User-Id") @NotNull final UUID userId,
+                               @RequestBody @NotNull final TaskRequest taskRequest){
+        return null;
+    }
+
+    @PutMapping("/{taskId}")
+    public TaskResponse update(@RequestHeader("X-User-Id") @NotNull final UUID userId,
+                               @PathVariable @NotNull final UUID taskId,
+                               @RequestBody @NotNull final TaskRequest taskRequest){
+        return null;
+    }
+
+    @DeleteMapping("/{taskId}")
+    public void delete(@RequestHeader("X-User-Id") @NotNull final UUID userId,
+                       @PathVariable @NotNull final UUID taskId){
+    }
+
+    @PutMapping("/done/{taskId}")
+    public TaskResponse done(@RequestHeader("X-User-Id") @NotNull final UUID userId,
+                             @PathVariable @NotNull final UUID taskId) {
+        return null;
+    }
 }
