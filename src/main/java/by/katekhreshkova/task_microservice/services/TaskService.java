cb843cd293f27abb74c274c26ae25d4cf371d5d6
Kate Khreshkova <katahrnskova@gmail.com>
2026-01-15 20:33:22 +1300

    feat(tasks): add createTask service method

--- src/main/java/by/katekhreshkova/task_microservice/services/TaskService.java
+++ src/main/java/by/katekhreshkova/task_microservice/services/TaskService.java
@@ -1,12 +1,31 @@
 package by.katekhreshkova.task_microservice.services;
 
+import by.katekhreshkova.task_microservice.contracts.req.TaskRequest;
+import by.katekhreshkova.task_microservice.contracts.resp.TaskResponse;
+import by.katekhreshkova.task_microservice.converters.TaskConverter;
+import by.katekhreshkova.task_microservice.models.Task;
+import by.katekhreshkova.task_microservice.models.TaskStatus;
 import by.katekhreshkova.task_microservice.repositories.TaskRepository;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.UUID;
+import static java.time.LocalDateTime.*;
 
 @Service
 @RequiredArgsConstructor
 public class TaskService {
     private final TaskRepository taskRepository;
+    private final TaskConverter taskConverter;
+
+    public TaskResponse createTask(@NotNull final UUID userId, @NotNull final TaskRequest taskRequest){
+        Task taskToSave = new Task();
+        taskToSave.setUserId(userId);
+        taskToSave.setTitle(taskRequest.title());
+        taskToSave.setDescription(taskRequest.description());
+        taskToSave.setStatus(TaskStatus.PENDING);
+        taskToSave.setCreatedAt(now());
+        return taskConverter.modelToContract(taskRepository.save(taskToSave));
+    }
 
 }
