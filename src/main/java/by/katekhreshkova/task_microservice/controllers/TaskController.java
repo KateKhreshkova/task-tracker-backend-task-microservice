d1bd1e0f9bacf55d10d739682107c6313d5eabd3
Kate Khreshkova <katahrnskova@gmail.com>
2026-01-17 20:23:05 +1300

    Feature: Add implementation for TaskController endpoints

--- src/main/java/by/katekhreshkova/task_microservice/controllers/TaskController.java
+++ src/main/java/by/katekhreshkova/task_microservice/controllers/TaskController.java
@@ -17,31 +17,32 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping
-    public List<TaskResponse> list(@RequestHeader("X-User-Id") @NotNull final UUID userId){
-        return null;
+    public List<TaskResponse> list(@RequestHeader("X-User-Id") @NotNull final UUID userId) {
+        return taskService.findAll(userId);
     }
 
     @PostMapping
     public TaskResponse create(@RequestHeader("X-User-Id") @NotNull final UUID userId,
-                               @RequestBody @NotNull final TaskRequest taskRequest){
-        return null;
+                               @RequestBody @NotNull final TaskRequest taskRequest) {
+        return taskService.createTask(userId, taskRequest);
     }
 
     @PutMapping("/{taskId}")
     public TaskResponse update(@RequestHeader("X-User-Id") @NotNull final UUID userId,
                                @PathVariable @NotNull final UUID taskId,
-                               @RequestBody @NotNull final TaskRequest taskRequest){
-        return null;
+                               @RequestBody @NotNull final TaskRequest taskRequest) {
+        return taskService.update(userId, taskId, taskRequest);
     }
 
     @DeleteMapping("/{taskId}")
     public void delete(@RequestHeader("X-User-Id") @NotNull final UUID userId,
-                       @PathVariable @NotNull final UUID taskId){
+                       @PathVariable @NotNull final UUID taskId) {
+        taskService.delete(userId, taskId);
     }
 
     @PutMapping("/done/{taskId}")
     public TaskResponse done(@RequestHeader("X-User-Id") @NotNull final UUID userId,
                              @PathVariable @NotNull final UUID taskId) {
-        return null;
+        return taskService.markDone(userId, taskId);
     }
 }
